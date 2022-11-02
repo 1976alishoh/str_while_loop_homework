d859146a6e49668bac770cfe1ea546cb9024ebda
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-11-02 09:51:14 +0300

    07 done

--- While07.py
+++ While07.py
@@ -6,4 +6,14 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a = ""
+    i = 0
+    while i<len(s):
+        if int(s[i])%2 == 0:
+            a += s[i] 
+        
+        i = i + 1
+    a = len(a)
+    return a
+print(main("56786543250"))
+print(main("123456"))
